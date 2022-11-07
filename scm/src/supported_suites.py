17af55e7c4424736812df511b5f1c804a4fc3dde
Man.Zhang <Man.Zhang@noaa.gov>
2022-11-07 13:09:19 -0700

    add nodeep for p8 suite

--- scm/src/supported_suites.py
+++ scm/src/supported_suites.py
@@ -1,1 +1,1 @@
-suites = ["SCM_GFS_v16","SCM_GFS_v17_p8","SCM_HRRR","SCM_RAP","SCM_RRFS_v1beta","SCM_WoFS_v0"]
+suites = ["SCM_GFS_v16","SCM_GFS_v17_p8","SCM_GFS_v17_p8_nodeep","SCM_HRRR","SCM_RAP","SCM_RRFS_v1beta","SCM_WoFS_v0"]
